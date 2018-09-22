7c7ee06bb77f6a6c9540d51ec68836115655ab1e
Steven Gresh <stvngrsh@gmail.com>
2018-09-22 04:18:12 -0400

    timer

--- src/components/PlateCounter.tsx
+++ src/components/PlateCounter.tsx
@@ -12,20 +12,17 @@ export interface PlateCounterProps {
 
 export interface PlateCounterState {
   plates: number[];
-  plateImages: JSX.Element[];
 }
 
 export default class PlateCounter extends React.Component<PlateCounterProps, PlateCounterState> {
   
   state: PlateCounterState = {
-    plates: [],
-    plateImages: []
+    plates: []
   }
   componentDidMount() {
-    let {plates, plateImages} = this.calculatePlates(this.props.weight);
+    let plates = this.calculatePlates(this.props.weight);
     this.setState({
-      plates: plates,
-      plateImages: plateImages
+      plates: plates
     });
   }
   
@@ -33,9 +30,7 @@ export default class PlateCounter extends React.Component<PlateCounterProps, Pla
     let plates: number[] = [];
     let remainingWeight = (weight - BAR) / 2;
     for(let plate of PLATES) {
-      console.log('plate :', plate, 'remWeight:', remainingWeight);
       let count = Math.floor(remainingWeight / plate);
-      console.log('count:', count);
       for(let i = 0; i < count; i++) {
         plates.push(plate);
       }
@@ -43,32 +38,7 @@ export default class PlateCounter extends React.Component<PlateCounterProps, Pla
         remainingWeight -= plate * count;
       }
     }
-    let plateImages: JSX.Element[] = [];
-    for(let plate of plates) {
-      plateImages.push(<Image source={require('./img.png')} />);
-
-      // switch(plate) {
-      //   case 45:
-      //     plateImages.push(<Image source={require('../assets/img/1x/45.png')} />);
-      //     break;
-      //   case 35:
-      //     plateImages.push(<Image source={require('../assets/img/1x/35.png')} />);
-      //     break;
-      //   case 25:
-      //     plateImages.push(<Image source={require('../assets/img/1x/25.png')} />);
-      //     break;
-      //   case 10:
-      //     plateImages.push(<Image source={require('../assets/img/1x/10.png')} />);
-      //     break;
-      //   case 5:
-      //     plateImages.push(<Image source={require('../assets/img/1x/5.png')} />);
-      //     break;
-      //   case 2.5:
-      //     plateImages.push(<Image source={require('../assets/img/1x/2_5.png')} />);
-      //     break;
-      // }
-    }
-    return {plates, plateImages};
+    return plates;
   }
 
   public render() {
@@ -76,8 +46,8 @@ export default class PlateCounter extends React.Component<PlateCounterProps, Pla
       <View style={styles.plates}>
         {this.state.plates.map((plate, index) => {
           return (
-            <View key={index} style={styles.plate}>
-              {this.state.plateImages[index]}
+            <View key={index} >
+              <Text style={styles.plate}>{plate}</Text>
             </View>
           );
         })}
@@ -104,7 +74,6 @@ const styles = StyleSheet.create({
     color: Theme.lightBlue
   },
   plate: {
-    justifyContent: 'center',
-    alignItems: 'center'
+    marginRight: 10
   }
 })
--- src/screens/LiftScreen.tsx
+++ src/screens/LiftScreen.tsx
@@ -3,13 +3,40 @@ import DataContainer from '../containers/DataContainer';
 import { View, StyleSheet } from 'react-native';
 import Template from '../Template';
 import { Lift, Set } from '../Types';
-import { Container, CheckBox, Card, CardItem, Body, Tabs, Tab, Content, Left, Icon, Button, Title, Text, Right } from 'native-base';
+import { Container, CheckBox, Card, CardItem, Body, Tabs, Tab, Content, Left, Icon, Button, Title, Text, Right, Toast } from 'native-base';
 import PlateCounter from '../components/PlateCounter';
+import { Theme } from '../Theme';
+import { Notifications, Permissions, Constants } from 'expo';
+
+const localNotification = {
+  title: 'Test',
+  body: 'Test', // (string) — body text of the notification.
+  ios: { // (optional) (object) — notification configuration specific to iOS.
+    sound: true // (optional) (boolean) — if true, play a sound. Default: false.
+  },
+android: // (optional) (object) — notification configuration specific to Android.
+  {
+    sound: true, // (optional) (boolean) — if true, play a sound. Default: false.
+    //icon (optional) (string) — URL of icon to display in notification drawer.
+    //color (optional) (string) — color of the notification icon in notification drawer.
+    sticky: false, // (optional) (boolean) — if true, the notification will be sticky and not dismissable by user. The notification must be programmatically dismissed. Default: false.
+    vibrate: true // (optional) (boolean or array) — if true, vibrate the device. An array can be supplied to specify the vibration pattern, e.g. - [ 0, 500 ].
+    // link (optional) (string) — external link to open when notification is selected.
+  }
+};
+
+let t = new Date();
+t.setSeconds(t.getSeconds() + 60);
+const schedulingOptions = {
+  time: t // (date or number) — A Date object representing when to fire the notification or a number in Unix epoch time. Example: (new Date()).getTime() + 1000 is one second from now.
+};
 
 export interface LiftScreenState {
   finishedWarmups: boolean[][],
   finishedSets: boolean[][],
-  finishedFSL: boolean[]
+  finishedFSL: boolean[],
+  timer: any,
+  timeRemaining: number
 }
 
 export interface LiftScreenProps {
@@ -29,7 +56,9 @@ export default class LiftScreen extends React.Component<LiftScreenProps, LiftScr
   state: LiftScreenState = {
     finishedWarmups: [],
     finishedSets: [],
-    finishedFSL: []
+    finishedFSL: [],
+    timer: null,
+    timeRemaining: 0
   }
 
   componentWillMount() {
@@ -71,6 +100,13 @@ export default class LiftScreen extends React.Component<LiftScreenProps, LiftScr
     });
   }
   
+  async componentDidMount() {
+    let result = await   
+    Permissions.askAsync(Permissions.NOTIFICATIONS);
+    if (Constants.isDevice && result.status === 'granted') {
+     console.log('Notification permissions granted.')
+    }
+  }
 
   getWeight = (percentage: number, lift: Lift) => {
     let multiplier = percentage / 100;
@@ -92,19 +128,48 @@ export default class LiftScreen extends React.Component<LiftScreenProps, LiftScr
     return Math.floor(amount / LOWEST_WEIGHT) * LOWEST_WEIGHT;
   }
 
-  finishSet = (liftIndex: number, setIndex: number, warmup: boolean) => {
+  finishSet = (liftIndex: number, setIndex: number, warmup: boolean, disabled: boolean) => {
     console.log('this.state.finishedWarmups :', this.state.finishedWarmups);
     console.log('liftIndex :', liftIndex);
     console.log('setIndex :', setIndex);
-    if(warmup) {
-      let {finishedWarmups} = this.state;
-      finishedWarmups[liftIndex][setIndex] = !finishedWarmups[liftIndex][setIndex];
-      this.setState({finishedWarmups: finishedWarmups});
+    if(!disabled) {
+      if(warmup) {
+        let {finishedWarmups} = this.state;
+        finishedWarmups[liftIndex][setIndex] = !finishedWarmups[liftIndex][setIndex];
+        this.setState({finishedWarmups: finishedWarmups});
+      } else {
+        let {finishedSets} = this.state;
+        finishedSets[liftIndex][setIndex] = !finishedSets[liftIndex][setIndex];
+        this.setState({finishedSets: finishedSets});
+      }
+      this.startTimer(60);
     } else {
-      let {finishedSets} = this.state;
-      finishedSets[liftIndex][setIndex] = !finishedSets[liftIndex][setIndex];
-      this.setState({finishedSets: finishedSets});
+      alert("You must finish the previous set!");
     }
+  }
+
+  tick = () => {
+    if(this.state.timeRemaining > 0) {
+      console.log(this.state.timeRemaining);
+      this.setState({
+        timeRemaining: this.state.timeRemaining - 1
+      });
+    } else {
+      clearInterval(this.state.timer);
+    }
+  }
+
+  startTimer = (timeRemaining: number) => {
+    let timer = setInterval(this.tick, 1000);
+    this.setState({timer, timeRemaining});
+    Toast.show({
+      duration: timeRemaining * 1000,
+      text: "Rest Timer: " + this.state.timeRemaining,
+      buttonText: "Done",
+      type: "success"
+    })
+    Notifications.scheduleLocalNotificationAsync(localNotification, schedulingOptions);
+
   }
 
   finishFSL = (index: number) => {
@@ -123,11 +188,21 @@ export default class LiftScreen extends React.Component<LiftScreenProps, LiftScr
           <Body>
             {sets.map((set, index) => { 
               let weight = this.getWeight(set.percent, lift);
+              let disabled = false;
+              if(warmup) {
+                disabled = index > 0 && this.state.finishedWarmups[liftIndex][index - 1] === false;
+              } else {
+                disabled = index > 0 && this.state.finishedSets[liftIndex][index - 1] === false;
+              }
               return (
                 <View style={styles.set} key={index}>
                   <Text style={styles.text}>{weight} x{set.reps}</Text>
-                  <PlateCounter weight={weight} />
-                  <CheckBox onPress={() => this.finishSet(liftIndex, index, warmup)} style={styles.checkbox} checked={warmup ? this.state.finishedWarmups[liftIndex][index] : this.state.finishedSets[liftIndex][index]} />
+                  <View style={styles.plates}>
+                    <PlateCounter weight={weight} />
+                  </View>
+                  <View style={styles.checkboxOuter} >
+                    <CheckBox onPress={() => this.finishSet(liftIndex, index, warmup, disabled)} style={disabled ? styles.checkBoxDisabled : styles.checkbox} checked={warmup ? this.state.finishedWarmups[liftIndex][index] : this.state.finishedSets[liftIndex][index]} />
+                  </View>
                 </View>
               );
             })}
@@ -192,6 +267,10 @@ export default class LiftScreen extends React.Component<LiftScreenProps, LiftScr
   }
 }
 
+const checkboxTheme = {
+  checkboxSize: 200
+}
+
 const styles = StyleSheet.create({
   card: {
     backgroundColor: '#303030'
@@ -205,21 +284,43 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between'
   },
   text: {
-    fontSize: 25
+    fontSize: 25,
+    width: "30%"
   },
   fslSets: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between'
   },
+  plates: {
+    width: "55%"
+  },
   fslCheckbox: {
-    width: 25,
-    height: 25,
+    padding: 5,
+    paddingLeft: 8,
+    borderRadius: 30,
+    width: 30,
+    height: 30,
     margin: 5
   },
+  checkboxOuter: {
+    width: "15%"
+  },
+  checkBoxDisabled: {
+    padding: 5,
+    paddingLeft: 8,
+    borderRadius: 30,
+    width: 30,
+    height: 30,
+    margin: 5,
+    borderColor: Theme.lightGray
+  },
   checkbox: {
-    width: 25,
-    height: 25,
+    padding: 5,
+    paddingLeft: 8,
+    borderRadius: 30,
+    width: 30,
+    height: 30,
     margin: 5
   }
 });
